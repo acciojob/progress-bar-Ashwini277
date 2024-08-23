460fa19e292b5f1859e414d6d03dfe167806eb31
Ashwini277 <devarushiashwini@gmail.com>
2024-08-23 10:43:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-//your JS code here. If required.
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+nextBtn.addEventListener('click', () => {
+    currentActive++;
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+
+    update();
+});
+
+prevBtn.addEventListener('click', () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+
+    update();
+});
+
+function update() {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    // Enable or disable the Previous button
+    if (currentActive === 1) {
+        prevBtn.disabled = true;
+    } else {
+        prevBtn.disabled = false;
+    }
+
+    // Enable or disable the Next button
+    if (currentActive === circles.length) {
+        nextBtn.disabled = true;
+    } else {
+        nextBtn.disabled = false;
+    }
+}
+
